cde4a182ba7639a8f093033ccbe9c33ee84124b2
Alvar Penning <post@0x21.biz>
2019-10-20 13:44:37 +0200

    Implement Writer for Modem

--- rf95/modem.go
+++ rf95/modem.go
@@ -2,6 +2,10 @@ package rf95
 
 import (
 	"bufio"
+	"encoding/hex"
+	"fmt"
+	"regexp"
+	"strconv"
 	"sync"
 
 	"github.com/tarm/serial"
@@ -11,7 +15,7 @@ type Modem struct {
 	device     string
 	serialPort *serial.Port
 	reader     *bufio.Reader
-	lock       sync.WaitGroup
+	readLock   sync.WaitGroup
 }
 
 func OpenModem(device string) (modem *Modem, err error) {
@@ -36,7 +40,7 @@ func OpenModem(device string) (modem *Modem, err error) {
 }
 
 func (modem *Modem) Read(p []byte) (int, error) {
-	modem.lock.Wait()
+	modem.readLock.Wait()
 
 	lineMsg, lineErr := modem.reader.ReadString('\n')
 	if lineErr != nil {
@@ -51,7 +55,32 @@ func (modem *Modem) Read(p []byte) (int, error) {
 	return copy(p, rxBytes), nil
 }
 
+func (modem *Modem) Write(p []byte) (int, error) {
+	modem.readLock.Add(1)
+	defer modem.readLock.Done()
+
+	msg := fmt.Sprintf("AT+TX=%s\n", hex.EncodeToString(p))
+	if _, err := modem.serialPort.Write([]byte(msg)); err != nil {
+		return 0, err
+	}
+
+	respMsg, respErr := modem.reader.ReadString('\n')
+	if respErr != nil {
+		return 0, respErr
+	}
+
+	respPattern := regexp.MustCompile(`^\+SENT (\d+) bytes\.\r\n$`)
+	respMatch := respPattern.FindStringSubmatch(respMsg)
+	if len(respMatch) != 2 {
+		return 0, fmt.Errorf("unexpected response: %s", respMsg)
+	} else if n, nErr := strconv.Atoi(respMatch[1]); nErr != nil {
+		return 0, nErr
+	} else {
+		return n, nil
+	}
+}
+
 func (modem *Modem) Close() error {
-	modem.lock.Wait()
+	modem.readLock.Wait()
 	return modem.serialPort.Close()
 }
